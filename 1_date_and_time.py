229580cd4f8196ab83db3777ff90d787b91eec8e
mks3011 <bmb3011@gmail.com>
2024-02-26 23:29:24 +0300

    in process

--- 1_date_and_time.py
+++ 1_date_and_time.py
@@ -7,13 +7,22 @@
 2. Превратите строку "01/01/20 12:10:03.234567" в объект datetime
 
 """
+from datetime import date, timedelta
+
 
 def print_days():
     """
     Эта функция вызывается автоматически при запуске скрипта в консоли
     В ней надо заменить pass на ваш код
     """
-    pass
+    today = date.today()
+    print("Сегодня", today.strftime('%d.%m.%Y'))
+
+    yesterday = today - timedelta(days=1)
+    print("Вчера", yesterday.strftime('%d.%m.%Y'))
+
+    days_before_30 = today - timedelta(days=30)
+    print("30 дней назад", days_before_30.strftime('%d.%m.%Y'))
 
 
 def str_2_datetime(date_string):
