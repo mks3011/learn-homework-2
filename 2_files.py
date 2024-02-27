81d3f1f85e22c6e1ec4ada381d4ab59e27c1b067
mks3011 <bmb3011@gmail.com>
2024-02-27 18:00:15 +0300

    done

--- 2_files.py
+++ 2_files.py
@@ -16,7 +16,25 @@ def main():
     Эта функция вызывается автоматически при запуске скрипта в консоли
     В ней надо заменить pass на ваш код
     """
-    pass
+
+    with open('referat.txt', 'r', encoding='utf-8') as ref:
+        # Прочитайте содержимое файла в переменную, подсчитайте длину получившейся строки
+        referat = ref.read()
+        print(referat)
+        print(len(referat))
+
+        # Подсчитайте количество слов в тексте
+        words = referat.split()
+        print(len(words))
+
+        # Замените точки в тексте на восклицательные знаки
+        new_referat = referat.replace(".", "!")
+        print(new_referat)
+
+        # Сохраните результат в файл referat2.txt
+    with open('referat2.txt', 'w', encoding='utf-8') as ref2:
+        ref2.write(new_referat)
+
 
 if __name__ == "__main__":
     main()
